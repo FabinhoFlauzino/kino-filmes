8c366217a18d6ae8622a59022fdfe8181bb953fd
Fabio Flauzino <fabio@mfrural.com.br>
2025-02-19 16:45:01 -0300

    Criando componente Grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import Flex from "@/components/template/Flex";
+import Grid from "@/components/template/Grid";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <Flex col>
+    <Grid numCols={2} column numRows={3}>
       <p className="w-20 h-20 bg-red-200">p1</p>
       <p className="w-20 h-20 bg-green-200">p2</p>
-    </Flex>
+      <p className="w-20 h-20 bg-red-200">p1</p>
+      <p className="w-20 h-20 bg-green-200">p2</p>
+      <p className="w-20 h-20 bg-red-200">p1</p>
+      <p className="w-20 h-20 bg-green-200">p2</p>
+      <p className="w-20 h-20 bg-red-200">p1</p>
+      <p className="w-20 h-20 bg-green-200">p2</p>
+    </Grid>
   );
 }
