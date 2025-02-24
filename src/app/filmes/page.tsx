346ae0d2bf553197f27d5d81ced35f9bf3c5d21a
Fabio Flauzino <fabio@mfrural.com.br>
2025-02-24 12:20:15 -0300

    Criando Hook para pegar dados da API e mostrando na tela

--- src/app/filmes/page.tsx
+++ src/app/filmes/page.tsx
@@ -1,7 +1,19 @@
+"use client"
+import Wrap from "@/components/template/Wrap"
+import useMovieAPI from "@/hooks/useMovieAPI"
+import { useEffect, useState } from "react"
+
 export default function Filmes() {
+  const [filmes, setFilmes] = useState<Filme[]>([])
+  const { getUltimosFilmes } = useMovieAPI()
+
+  useEffect(() => {
+    getUltimosFilmes().then(setFilmes)
+  }, [])
+
   return (
-    <div className="text-6xl font-bold font-white">
-      Filmes
-    </div>
+    <Wrap>
+      {JSON.stringify(filmes, null, 6)}
+    </Wrap>
   )
 }
