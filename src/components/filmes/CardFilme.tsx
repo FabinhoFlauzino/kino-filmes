5c10a41ab65be562bbf5f5d0d7650521fca1f0c2
Fabio Flauzino <fabio@mfrural.com.br>
2025-02-26 15:40:47 -0300

    Criando componente NOta

--- src/components/filmes/CardFilme.tsx
+++ src/components/filmes/CardFilme.tsx
@@ -4,6 +4,8 @@ import ImagemComFallback from "../template/ImagemComFallback"
 import Titulo from "../template/Titulo"
 import Wrap from "../template/Wrap"
 import Generos from "./Generos"
+import Nota from "./Nota"
+import mergeClasses from "@/utils/mergeClasses"
 
 interface CardFilmeProps {
   filme: Filme
@@ -12,10 +14,11 @@ interface CardFilmeProps {
 
 export default function CardFilme({ filme, className }: CardFilmeProps) {
   return (
-    <Wrap className="
-        rounded-2xl h-60 max-h-60 bg-black border border-white/[0.2]
-        group-hover:boder-kino relative z-20
-      "
+    <Wrap className={mergeClasses(
+      `rounded-2xl h-60 max-h-60 bg-black border 
+      border-white/[0.2] group-hover:boder-kino relative z-20`,
+      className
+    )}
     >
       <ImagemComFallback
         url={filme.linkImagemFundo}
@@ -25,9 +28,10 @@ export default function CardFilme({ filme, className }: CardFilmeProps) {
         <FilmSlate className="w-1/2 h-2/3 transition-all" />
       </ImagemComFallback>
       <Flex col className="h-60 z-50 justify-between py-10 px-2">
-        <Titulo texto={filme.titulo} pequeno alinhar="left" />
+        <Titulo texto={filme.titulo} pequeno alinhar="left" className="m-auto"/>
         <Flex col className="justify-start items-start w-full">
           <Generos idFilme={filme.id} />
+          <Nota nota={filme.nota} />
         </Flex>
       </Flex>
     </Wrap>
